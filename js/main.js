26d6c166ca2d158d436474b50afcdc8dfef76106
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-13 11:35:52 +0200

    Js refactoring

--- js/main.js
+++ js/main.js
@@ -112,12 +112,14 @@ const icons = [
   // Milestone 1
 // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
 
-// var icon = document.getElementById("icon");
-// var iconName = document.getElementById("icon-name");
+var iconsContainer = document.getElementById("container");
 
 icons.forEach(element => {
-    document.getElementById("container").innerHTML += `<div class="card">
-    <div class="icon ${element.category}"><i class="${element.family} ${element.prefix}${element.name}"></i></div>
-    <p class="icon-name">${element.name}</p>
+    //destrutturazione
+    let {name, family, prefix, category} = element;
+
+    iconsContainer.innerHTML += `<div class="card">
+    <div class="icon ${category}"><i class="${family} ${prefix}${name}"></i></div>
+    <p class="icon-name">${name}</p>
 </div>`
 });
